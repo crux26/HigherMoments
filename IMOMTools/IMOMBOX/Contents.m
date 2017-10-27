5731c07620f741ca90cbf69caaf60fc7369b0e5a
crux26 <crux26@business.kaist.ac.kr>
2017-10-27 09:44:46 +0900

    Implied Option Moments calculation file.

---
+++ IMOMTools/IMOMBOX/Contents.m
@@ -0,0 +1,8 @@
+% IMOMBOX
+%
+% Files
+%   mOption2price     - compute prices of moment contracts from calls and puts
+%   mOption2stat      - compute implied standardized moments from calls and puts
+%   mVol2price        - compute prices of moment contracts given volatility smile
+%   mVol2stat         - implied standardized moments given volatility smile
+
