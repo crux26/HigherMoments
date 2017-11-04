ba19b6428adfd01cd144b7851e03c36a56836a5a
crux26 <crux26@business.kaist.ac.kr>
2017-11-04 21:40:39 +0900

    Files removed.

--- IV_constMaturity.m
+++
@@ -1,4 +0,0 @@
-function [IV_C_constMaturity, IV_P_constMaturity] = IV_constMaturity(constMaturity, t1, CallIV1, PutIV1, t2, CallIV2, PutIV2)
-
-IV_C_constMaturity = interp1([t1; t2], [CallIV1; CallIV2], constMaturity, 'linear', 'extrap');
-IV_P_constMaturity = interp1([t1; t2], [PutIV1; PutIV2], constMaturity, 'linear', 'extrap');
