dd897d6f34f431f4ae4bc9fd1692db2315ffb33a
crux26 <crux26@business.kaist.ac.kr>
2017-11-16 19:30:21 +0900

    Fixed coeff_MA1: from AR to MA
    Prior value AR was an error.

--- main_functions/myARMA11.m
+++ main_functions/myARMA11.m
@@ -1,13 +1,15 @@
 function [tbl_coeff, tbl_SE, tbl_t, res] = myARMA11(yvar)
-ARMA11 = arima('ARLags', 1, 'MALags', 1); fprintf('\nEquiv. to EViews.SKEW(-1)\n');
-% ARMA11 = regARIMA(1,0,1); fprintf('\nEquiv. to Eviews.AR(1)\n');
+% Cheked res is more or less the same of EViews' residual for running ARMA11.
+% Hence, this is not a software problem. They had run a different model.
+ARMA11 = arima('ARLags', 1, 'MALags', 1); fprintf('\nUsing arima(). This is equiv. to EViews.SKEW(-1)\n');
+% ARMA11 = regARIMA(1,0,1); fprintf('\nUsing regARIMA(). This is equiv. to Eviews.AR(1)\n');
 
-Display='params';   % off, full, diagnostics, iter, params(default)
+Display='off';   % off, full, diagnostics, iter, params(default)
 [Est, EstCov, ~, ~] = estimate(ARMA11, yvar, 'Display', Display);
 [res,~,~] = infer(Est, yvar);
 diag_ = sqrt(diag(EstCov));
 coeff_AR1 = cell2mat(Est.AR(:));  % AR(1)
-coeff_MA1 = cell2mat(Est.AR(:));  % MA(1)
+coeff_MA1 = cell2mat(Est.MA(:));  % MA(1)
 coeff_SE = diag_(2:end-1);   % AR(1).SE() & MA(1).SE()
 coeff_t = [coeff_AR1; coeff_MA1] ./ coeff_SE;
 
