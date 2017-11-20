882e00b310e1bb3ba262e69a0185de1f1a0552da
crux26 <crux26@business.kaist.ac.kr>
2017-11-20 11:18:21 +0900

    Uses myblscall/put now.

--- data/codes/functions/IVextrap_ByExdate_mat.m
+++ data/codes/functions/IVextrap_ByExdate_mat.m
@@ -3,20 +3,22 @@ function [T] = IVextrap_ByExdate_mat(OpData, TTM)
 [exdate_ByDate, idx_exdate_ByDate] = unique(OpData(:,2));
 
 idx_exdate_ByDate = [idx_exdate_ByDate; length(OpData(:,2)) + 1];
-m = 1000;
+m = 1e+3;   % The number of moneyness grids.
 T = [];
 for i=1:length(exdate_ByDate)
     tmpIdx = idx_exdate_ByDate(i) : (idx_exdate_ByDate(i+1)-1);
-    moneyness_grid = linspace(0.01, 3, m);
+    moneyness_grid = linspace(1e-4, 3, m); % 0.01% seems to be 1%, but trusting the paper's value.
+    % In fact, they're more or less the same. 
+    % find(min(abs(moneyness_grid-1)))==332, 334 for 1e-2, 1e-4 case, respectively.
     moneyness_min = min(OpData(tmpIdx,17)); moneyness_max = max(OpData(tmpIdx,17));
     
-    idx_middle= find(moneyness_grid >= moneyness_min & moneyness_grid <= moneyness_max);
-    IV_extrap = zeros(1000,1);    
+    idx_middle = find(moneyness_grid >= moneyness_min & moneyness_grid <= moneyness_max);
+    IV_extrap = zeros(m,1);
     IV_extrap(idx_middle) = interp1(OpData(tmpIdx,17), OpData(tmpIdx,6), moneyness_grid(idx_middle), 'pchip');
-    
+    % OpData(:,17): moneyness, OpData(:,6): IV
     idx_max = find(moneyness_grid > moneyness_max);
     idx_min = find(moneyness_grid < moneyness_min);
-    
+    % Constant IV for ITM part.
     IV_extrap(idx_max) = IV_extrap( min(idx_max) - 1);
     IV_extrap(idx_min) = IV_extrap( max(idx_min) + 1);
     
@@ -30,13 +32,14 @@ for i=1:length(exdate_ByDate)
         error('TTM_.len()~=1.');
     end
     
-    K = moneyness_grid' * S;
+    K = S ./ moneyness_grid';
     
     if OpData(1,20) == 1
-        [~, OpPrice] = blsprice(S, moneyness_grid'*S, r, TTM_, IV_extrap);
+        OpPrice = myblsput(S, K, r, TTM_, IV_extrap, q);
     else
-        [OpPrice, ~] = blsprice(S, moneyness_grid'*S, r, TTM_, IV_extrap);
+        OpPrice = myblscall(S, K, r, TTM_, IV_extrap, q);
     end
+
     
     T_ = table(repmat(OpData(1,1),m,1), repmat(exdate_ByDate(i), m, 1), ...
         K, IV_extrap, moneyness_grid', repmat(S,m,1), repmat(r,m,1), ...
